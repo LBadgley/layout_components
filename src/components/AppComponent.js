dd6467c0bc883ce878a0e44925423c0b212c2600
Laura Badgley <l.badgleyy@gmail.com>
2019-05-13 16:07:04 -0700

    simple color is rendering test not writen yet

---
+++ src/components/AppComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Color from './ColorComponent';
+
+export default function App() {
+  return (
+    <>
+    <Color />
+    </>
+  );
+}
---
+++ src/components/ColorComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Color() {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>Stil de Grain Yellow</dd>
+
+      <dt>Hex</dt>
+      <dd>#F4D35E</dd>
+
+      <dt>RGB</dt>
+      <dd>R: 244</dd>
+      <dd>G: 211</dd>
+      <dd>B: 94</dd>
+    </dl>
+  );
+}
