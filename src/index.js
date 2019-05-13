e6d1d2080be16b67c8da17d5b638f5eb31ddfc35
Laura Badgley <l.badgleyy@gmail.com>
2019-05-13 13:28:28 -0700

    updated config for react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('wassup')
+// append a ul
+// append a li with 'spot', 'rover', 'bingo', 'joe'
+import styles from './index.css';
+
+const root = document.getElementById('root');
+const ul = document.createElement('ul');
+ul.className = styles.dogs;
+root.appendChild(ul);
+
+const bingo = document.createElement('li');
+bingo.textContent = 'bingo';
+
+const rover = document.createElement('li');
+rover.textContent = 'rover';
+
+const spot = document.createElement('li');
+spot.textContent = 'spot';
+
+const joe = document.createElement('li');
+joe.textContent = 'joe';
+
+ul.appendChild(bingo);
+ul.appendChild(rover);
+ul.appendChild(spot);
+ul.appendChild(joe);
+
